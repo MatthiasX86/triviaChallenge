a77e5c34c0f2165fb3f09eebe99737f872b9395e
Matt <28681206+MatthiasX86@users.noreply.github.com>
2021-02-28 22:51:03 -0500

    created app intro desktopview

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,16 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
-import {Colors} from '../styles/mixins'
+import {Colors, Transitions} from '../styles/mixins'
+import {IHandle} from '../types/index';
 
 const Element = styled.button`
   margin: 0 auto;
+  transition: all 300ms ease-out;
+  ${Transitions.standardHover};
 `
 
-export const Button: FunctionComponent = ({ children }) => (
-  <Element>
+export const Button: FunctionComponent<IHandle> = ({ clickHandle, children }) => (
+  <Element onClick={clickHandle}>
     <span>{children}</span>
   </Element>
 )
--- src/views/intro.tsx
+++ src/views/intro.tsx
@@ -1,6 +1,10 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import {Col} from 'react-bootstrap';
 import styled from 'styled-components';
+import {Assets} from '../assets/assets';
+import {Button} from '../components/button';
+import {Animations, Transitions, UIStyles} from '../styles/mixins';
+import { IActive, IHandle } from '../types/index';
 
 /* ====== Containers ====== */
 
@@ -12,12 +16,46 @@ const SectionContainer = styled.section`
   position: relative;
   height: 700px;
   width: 100%;
-  border: 1px solid red;
 `
 
 const StartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 100px;
   bottom: 50px;
-  border: 1px solid green;
+`
+const InstructionsContainer = styled.div<IActive>`
+  height: ${ ({ active }) => active
+    ? '300px'
+    : 'auto'
+  };
+  width: ${ ({ active }) => active
+    ? '400px'
+    : '180px'
+  };
+  flex-direction: ${ ({ active }) => active
+    ? 'column'
+    : 'row'
+  };
+  padding: ${ ({ active }) => active
+    ? '80px 30px 80px;'
+    : '10px 20px'
+  };
+  position: absolute;
+  bottom: 0;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 30px;
+  border-radius 30px;
+  box-shadow: ${UIStyles.boxShadow};
+  background-color: rgba(0, 0, 0, 0.86);
+  border-radius: 30px;
+
+  transition: all 300ms ease-out;
+  ${Transitions.standardHover};
 `
 
 /* ====== UI Elements ====== */
@@ -28,13 +66,108 @@ const Title = styled.h1`
 const Subtitle = styled.h2`
 `
 
+const InfoIcon = styled.img<IActive>`
+  display: ${ ({ active }) => active
+    ? 'none'
+    : 'initial'
+  };
+  height: 25px;
+  width: 25px;
 
-export const Intro: FunctionComponent = ({ children }) => (
+  animation-delay: 400ms;
+  ${Animations.fadeIn}
+`
+
+const CloseIcon = styled.img<IActive>`
+  display: ${ ({ active }) => active
+    ? 'initial'
+    : 'none'
+  };
+
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  height: 25px;
+  width: 25px;
+
+  animation-delay: 400ms;
+  ${Animations.fadeIn}
+
+`
+
+/* ====== Text Elements ====== */
+
+const HowSmallText = styled.h4<IActive>`
+  display: ${ ({ active }) => active
+    ? 'none'
+    : 'initial'
+  };
+
+  animation-delay: 400ms;
+  ${Animations.fadeIn}
+`
+
+const HowLargeText = styled.h3<IActive>`
+  display: ${ ({ active }) => active
+    ? 'initial'
+    : 'none'
+  };
+
+  animation-delay: 400ms;
+  ${Animations.fadeIn}
+`
+
+/* ====== Components ====== */
+
+
+const HowToPlay: FunctionComponent = () => { 
+
+  const [isActive, setActive] = useState(false);
+
+  return (
+    <InstructionsContainer
+      onClick={() => setActive(!isActive)}
+      active={isActive}
+    >
+      <InfoIcon 
+        src={Assets.Info}
+        active={isActive}
+      />
+      <CloseIcon
+        src={Assets.CloseIcon}
+        active={isActive}
+      />
+      {isActive
+        ? (
+          <>
+            <HowLargeText active={isActive}>
+              You will be presented with 10 true or false questions
+            </HowLargeText>
+            <HowLargeText active={isActive}>
+              Think you can score 100%?
+            </HowLargeText>
+          </>
+        )
+        : (
+          <HowSmallText active={isActive}>
+            How to play
+          </HowSmallText>
+        )
+      }
+    </InstructionsContainer>
+  )
+ }
+
+export const Intro: FunctionComponent<IHandle> = ({ clickHandle }) => (
   <Col lg={{ span: 10, offset: 1}}>
     <SectionContainer>
       <Title>Trivia<br/>Challenge</Title>
       <StartContainer>
         <Subtitle>Welcome to trivia Challenger!</Subtitle>
+        <Button clickHandle={clickHandle}>
+          Let's play!
+        </Button>
+        <HowToPlay />
       </StartContainer>
     </SectionContainer>
   </Col>
